45e17ff809280933398cefb7f5267e956594f2f4
TTH-Dev <161312731+TTH-Dev@users.noreply.github.com>
2024-03-01 18:44:56 +0530

    fixing all the issue
    fixing all the issue

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,11 @@
+import 'dart:io';
+
 import 'package:example/saved_image_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:whatsapp_story_editor/whatsapp_story_editor.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
 
 void main() {
   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
@@ -10,7 +14,7 @@ void main() {
     statusBarIconBrightness: Brightness.light,
     systemNavigationBarIconBrightness: Brightness.light,
   ));
-  runApp( const MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatefulWidget {
@@ -21,8 +25,56 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  static Color mattBlack = const Color.fromARGB(255, 25, 40, 46);
-  static Color green3 = const Color.fromARGB(255, 7, 191, 139);
+  final List<String> imageUrl = [
+    'assets/cake.jpg',
+    'assets/image1.jpg',
+    'assets/image2.jpg',
+    'assets/persion.jpeg',
+    // Add more image URLs as needed
+  ];
+  var data;
+  List<File?> imageUrls = [];
+  Future<File?> getImageFileFromAsset(String assetPath) async {
+    try {
+      final ByteData byteData = await rootBundle.load(assetPath);
+      final List<int> byteList = byteData.buffer.asUint8List();
+
+      // Get the temporary directory path
+      final String tempPath = (await getTemporaryDirectory()).path;
+
+      // Extract the file name from the asset path
+      final String fileName = basename(assetPath);
+
+      // Create the temporary file with the correct path
+      final File tempFile = File('$tempPath/$fileName');
+
+      // Write the bytes to the file
+      await tempFile.writeAsBytes(byteList, flush: true);
+
+      return tempFile;
+    } catch (e) {
+      print('Error loading asset: $e');
+      return null;
+    }
+  }
+
+  Future<void> loadImageFiles() async {
+    for (var i = 0; i < imageUrl.length; i++) {
+      final File? file = await getImageFileFromAsset(imageUrl[i]);
+      setState(() {
+        imageUrls.add(file);
+      });
+    }
+    print(imageUrls);
+
+    // Now, you can use the populated imageFiles list as needed.
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    loadImageFiles();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,38 +82,15 @@ class _MyAppState extends State<MyApp> {
         debugShowCheckedModeBanner: false,
         home: Scaffold(
           backgroundColor: Colors.black,
-          floatingActionButton: Builder(builder: (context) {
-            return FloatingActionButton(
-              onPressed: () async {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const WhatsappStoryEditor()),
-                ).then((whatsappStoryEditorResult) {
-                  if (whatsappStoryEditorResult != null) {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => SavedImageView(
-                                image: whatsappStoryEditorResult.image,
-                                caption: whatsappStoryEditorResult.caption,
-                              )),
-                    );
-                  }
-                });
-              },
-              child: Container(
-                height: 60,
-                width: 60,
-                decoration:
-                    BoxDecoration(color: green3, shape: BoxShape.circle),
-                child: const Icon(
-                  Icons.camera_alt,
-                  color: Colors.white,
-                  size: 20,
+          body:data==null? Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2, // Number of columns
+                  crossAxisSpacing: 8.0, // Spacing between columns
+                  mainAxisSpacing: 8.0, // Spacing between rows
                 ),
-<<<<<<< Updated upstream
-=======
                 itemCount: imageUrls.length,
                 shrinkWrap: true,
                 itemBuilder: (context, index) {
@@ -74,7 +103,17 @@ class _MyAppState extends State<MyApp> {
                                   image: imageUrls[index],
                                 )),
                       ).then((whatsappStoryEditorResult) {
-                        
+                        // if (whatsappStoryEditorResult != null) {
+                        //   Navigator.push(
+                        //     context,
+                        //     MaterialPageRoute(
+                        //         builder: (context) => SavedImageView(
+                        //               image: whatsappStoryEditorResult.image,
+                        //               caption:
+                        //                   whatsappStoryEditorResult.caption,
+                        //             )),
+                        //   );
+                        // }
                         setState(() {
                           data = whatsappStoryEditorResult;
                         });
@@ -105,64 +144,30 @@ class _MyAppState extends State<MyApp> {
                     fit: BoxFit.contain,
                   ),
                   ),
->>>>>>> Stashed changes
               ),
-            );
-          }),
-          body: Container(
-            color: mattBlack,
-            padding: const EdgeInsets.only(top: 50, left: 18, right: 18),
-            height: 140,
-            width: double.infinity,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(
-                  "WhatsApp",
-                  style: TextStyle(
-                    fontWeight: FontWeight.w400,
-                    color: Colors.white38,
-                    fontSize: 21,
+              const SizedBox(height: 30),
+              Text(
+                  data.caption ?? "",
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(color: Colors.white),
+              ),
+              const SizedBox(height: 30),
+            ],
                   ),
-                ),
-                const Spacer(),
-                const Expanded(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        "Chats",
-                        style: TextStyle(
-                          fontWeight: FontWeight.w400,
-                          color: Colors.white38,
-                          fontSize: 16,
-                        ),
-                      ),
-                      Center(
-                        child: Text(
-                          "Status",
-                          style: TextStyle(
-                            fontWeight: FontWeight.w400,
-                            color: Colors.white38,
-                            fontSize: 16,
-                          ),
-                        ),
-                      ),
-                      Text(
-                        "Calls",
-                        style: TextStyle(
-                          fontWeight: FontWeight.w400,
-                          color: Colors.white38,
-                          fontSize: 16,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                const SizedBox(height: 12),
-                Center(child: Container(height: 3, width: 130, color: green3))
-              ],
-            ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 50),
+                    child: Align(
+                      alignment: Alignment.topRight,
+                      child: IconButton(onPressed: () {
+                        print("object");
+                        setState(() {
+                          
+                        });
+                        data=null;
+                      }, icon: Icon(Icons.clear,color: Colors.white,)),
+                    ),
+                  )
+                ],
           ),
         ));
   }
